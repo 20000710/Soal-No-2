4e26c18af79267b1ad3db994ebf248598797f5fa
zaki khairi <45307755+20000710@users.noreply.github.com>
2022-08-03 17:00:30 +0700

    Update soal-3.js

--- week-2/soal-3.js
+++ week-2/soal-3.js
@@ -5,31 +5,35 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray, callback) => {
     if (validator !== undefined){
         console.log(validator)
     }
-
 }
 
 // function callback validator
 const validator = (awal, akhir, dataArray) => {
     let arr = []
-    
-    try {
-        if(awal < akhir && dataArray.length > 5){
-            let sort = dataArray.sort((a, b) => {
-                return a-b;
-            })
-    
-            sort.map(number => {
-                if(number > awal && number < akhir)
-                arr.push(number)              
-            })
-        } 
-        else if(awal > akhir) throw "Nilai akhir harus lebih besar dari nilai awal"
-        else if(dataArray.length <= 5) throw "Jumlah angka dalam dataArray harus lebih dari 5"
-    } catch (error) {
-        console.log(error)
-    }
 
-    return arr;
+    if(awal < akhir && dataArray.length > 5){
+        let sort = dataArray.sort((a, b) => {
+            return a-b;
+        })
+
+        sort.map(number => {
+            if(number > awal && number < akhir)
+            arr.push(number)              
+        })
+
+        try {
+            if(awal > akhir) throw "Nilai akhir harus lebih besar dari nilai awal"
+            else if(dataArray.length <= 5) throw "Jumlah angka dalam dataArray harus lebih dari 5"
+            else if(arr.length === 0) {
+                arr += ""
+                throw "Nilai tidak ditemukan"
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        return arr
+    } 
 }
 
-seleksiNilai(3, 15, [2, 25, 4, 14, 17, 30, 8], validator)
+seleksiNilai(5, 17, [2, 25, 4, 1, 30, 18], validator)
